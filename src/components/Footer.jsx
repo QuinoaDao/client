deba1cdb7e90578b5a5ce639d66f47f6a356cb42
Seomingyun <aa123490@gmail.com>
2022-09-05 04:21:00 +0900

    add footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ export const Footer = () => {
         <div className="qui_footer">
           <a className="footer_txtBtn">Docs</a>
           <a className="footer_txtBtn">Governance</a>
-          <a className="channel_link twitter">a</a>
+          <a className="channel_link twitter" href="https://twitter.com/QuinoaProtocol" target="_blank" rel="noopener noreferrer">a</a>
           <a className="channel_link telegram">a</a>
-          <a className="channel_link discord">a</a>
-          <a className="coachBtn">need help?</a>
+          <a className="channel_link discord" href="https://discord.gg/uwF8MmUjMk" target="_blank" rel="noopener noreferrer">a</a>
+          <a className="coachBtn" href="mailto:quinoa.protocol@gmail.com">need help?</a>
         </div>
       </footer>
     )

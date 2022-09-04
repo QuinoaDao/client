d4ea42906b3fab1a6938f5b245dc7fcf7e79c348
JAE YEON KIM <65452447+jaethewiederholen@users.noreply.github.com>
2022-09-05 03:43:45 +0900

    fix header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ export const Footer = () => {
     return(
         <footer id="footer_wrap">
         <div className="qui_footer">
-          <a className="footer_txtBtn">Docs</a>
-          <a className="footer_txtBtn">Governance</a>
+          <a href="https://docs.quinoa.investments/"className="footer_txtBtn">Docs</a>
+          <a className="footer_txtBtn hide">Governance</a>
           <a className="channel_link twitter">a</a>
           <a className="channel_link telegram">a</a>
           <a className="channel_link discord">a</a>
